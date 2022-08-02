d60e96249b0de86d581636a4ef8b9b35f71bad90
Isaac Augusto <isaac.augusto@exitgames.com>
2022-08-01 22:49:07 -0300

    Refatorando classe menu lógica de menu

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
+import java.io.Console;
+import java.nio.channels.ClosedSelectorException;
 import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
+		Menu mainMenu = Menu.MainMenu();
+		int op = -1;
+		while (op != 4){
+			op = mainMenu.getSelection();
+			Menu.WritePopUp(String.format("Opção %s selecionada", op));
+		}
+
+		System.out.println("Fechando aplicativo.");
 	}
 
 }

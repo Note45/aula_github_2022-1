b5c3c4341c81f3f0ac27d0a9af1a0c31f7732e4c
Isaac Augusto <isaac.augusto@exitgames.com>
2022-08-01 22:13:02 -0300

    Adicionando classe Conta #1

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+	private static List<Account> _accounts = new ArrayList<Account>();
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));

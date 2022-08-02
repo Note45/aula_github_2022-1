e9ff0a56528260af1e7068162d013f34b34bd0d4
Note45 <antonion.2048@gmail.com>
2022-08-02 00:03:29 -0300

    Adiciona funcao de deposito na conta

--- Main.java
+++ Main.java
@@ -4,17 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-	private static List<Account> _accounts = new ArrayList<Account>();
-
 	public static void main(String[] args) {
 		Menu mainMenu = Menu.MainMenu();
 		int op = -1;
-		while (op != 4){
+
+		while (true){
 			op = mainMenu.getSelection();
-			Menu.WritePopUp(String.format("Opção %s selecionada", op));
 		}
-
-		System.out.println("Fechando aplicativo.");
-	}
-
+	}	
 }
--- Menu.java
+++ Menu.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Menu {
 	private String title;
 	private List<String> options;
+	private List<Account> accounts = new ArrayList<Account>();
 
 	public Menu(List<String> options) {
 		this.title = "Menu";
@@ -21,13 +22,14 @@ public class Menu {
 	}
 
 	public static Menu MainMenu() {
-		return new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+		return new Menu("Menu Principal", Arrays.asList("Sair", "Criar Conta", "Depositar na conta"));
 	}
 
 	public int getSelection() {
 		int op = 0;
+
 		while (op==0){
-			System.out.println(title+"\n");
+			System.out.println("\n"+title+"\n");
 			int i=1;
 			for (String option : options) {
 				System.out.println(i++ + " - " + option);
@@ -36,31 +38,74 @@ public class Menu {
 			System.out.println("Informe a opcao desejada. ");
 			Scanner s = new Scanner(System.in);
 			String str = s.nextLine();
+			
 			try {
 				op = Integer.parseInt(str);
-			}
-			catch (NumberFormatException e) {
-				op =0;
-			}
 
-			ClearConsole();
+				switch (op) {
+					case 1:
+						System.exit(0);
+					break;
+					case 2:
+						Scanner input = new Scanner(System.in);
+						System.out.println("Nome completo do titular: ");
+						String accountName = input.next();
+						System.out.println("Tipo[Corrente, Poupanca]: ");
+						String accountType = input.next();
+
+						int newAccountId = this.accounts.size();
+						
+						Account accountCreated = new Account();
+						accountCreated.Create(accountName, accountType, newAccountId);
+						
+						this.accounts.add(accountCreated);
+						System.out.println("\n"+ "Conta adicionada com sucesso!");
+						System.out.println("Conta: ");
+						System.out.println("Id: " + accountCreated.ID);
+						System.out.println("Nome titular: " + accountCreated.CompleteName);
+						System.out.println("Bloqueada: " + accountCreated.Blocked);
+						System.out.println("Tipo: " + accountCreated.Type);
+						System.out.println("Saldo: " + accountCreated.Money);
+					break;
+					case 3:
+						Scanner inputId = new Scanner(System.in);
+						System.out.println("Digite o id da conta: ");
+						int accountId = inputId.nextInt();
+						System.out.println("Digite o valor do deposito(Ex: 100.10): ");
+						double depositValue = inputId.nextDouble();
+
+						for (Account account : this.accounts) {
+							if (account.ID == accountId) {
+								account.Deposit(depositValue);
+
+								System.out.println("\n"+ "Deposito realizado com sucesso!");
+								System.out.println("Saldo atual da conta: " + account.Money);
+							}
+    				}
+					break;
+					default:
+						System.out.println("Opcao errada!");
+				}
+			} catch (NumberFormatException e) {
+				op = 0;
+			}
 
 			if (op>=i){
 				System.out.println("Opcao errada!");
 				System.console().readLine();
 				ClearConsole();
-				op=0;
+				op = 0;
 			}
 
 		}
 		return op;
 	}
 
-	private static void ClearConsole(){
+	private static void ClearConsole() {
 		System.out.print("\033\143");
 	}
 
-	public static void WritePopUp(String message){
+	public static void WritePopUp(String message) {
 		ClearConsole();
         System.out.println(message);
 		System.console().readLine();
